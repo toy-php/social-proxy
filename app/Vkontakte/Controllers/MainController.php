47bafd98459b397112a4ad939ad69dd2b887151a
toy-php <seymus007@gmail.com>
2017-05-25 05:38:00 +0300

    tmp

--- app/Vkontakte/Controllers/MainController.php
+++ app/Vkontakte/Controllers/MainController.php
@@ -11,25 +11,67 @@ use Psr\Http\Message\ServerRequestInterface;
 class MainController extends Controller
 {
 
+    /**
+     * Идентификатор Вашего приложения.
+     * @var string
+     */
     protected $clientId ='6044494';
 
+    /**
+     * Авторизация в соц. сети
+     * @param ServerRequestInterface $request
+     * @param Response $response
+     * @param \Proxy $proxy
+     * @return Response
+     */
     public function authAction(ServerRequestInterface $request,
                                Response $response,
                                \Proxy $proxy)
     {
+        /*
+            Указывает тип отображения страницы авторизации. Поддерживаются следующие варианты:
+                page — форма авторизации в отдельном окне;
+                popup — всплывающее окно;
+                mobile — авторизация для мобильных устройств (без использования Javascript)
+            Если пользователь авторизуется с мобильного устройства, будет использован тип mobile.
+         */
         $display ='page';
+
+        /*
+            Адрес, на который будет передан code (домен указанного адреса должен соответствовать
+            основному домену в настройках приложения и перечисленным значениям
+            в списке доверенных redirect uri — адреса сравниваются вплоть до path-части).
+         */
         $redirectUri = $request->getUri()
             ->withPath('/vk/callback')
             ->withQuery('');
+        /*
+            Битовая маска настроек доступа приложения,
+            которые необходимо проверить при авторизации пользователя и запросить отсутствующие.
+            смотреть: https://vk.com/dev/permissions
+         */
         $scope = 'email';
+        /*
+             Тип ответа, который Вы хотите получить.
+         */
         $responseType = 'code';
+
+        /*
+            Версия API, которую Вы используете.
+            смотреть: https://vk.com/dev/versions
+         */
         $oauthVersion = '5.64';
 
         /** @var SessionStorage $session */
         $session = $proxy['session'];
         $query = $request->getQueryParams();
+
+        // Адрес переадресации при успешной авторизации
         $session->set('sessionRedirect', isset($query['redirect']) ? $query['redirect'] : '');
+
+        // Адрес переадресации при возникновении ошибки
         $session->set('sessionError', isset($query['error']) ? $query['error'] : '');
+
         $url = (new Uri('https://oauth.vk.com/authorize'))
             ->withQuery(http_build_query([
                 'client_id' => $this->clientId,
@@ -42,13 +84,33 @@ class MainController extends Controller
         return $response->withHeader('Location', $url);
     }
 
+    /**
+     * Получение токена
+     * @param ServerRequestInterface $request
+     * @param Response $response
+     * @param \Proxy $proxy
+     * @return Response
+     */
     public function callbackAction(ServerRequestInterface $request,
                                Response $response,
                                \Proxy $proxy)
     {
+        /*
+            Защищенный ключ Вашего приложения (указан в настройках приложения)
+         */
         $clientSecret = '7LONJuDHTdVIbBz5JgwZ';
+
+        /*
+            URL, который использовался при получении code на первом этапе авторизации.
+            Должен быть аналогичен переданному при авторизации.
+         */
         $redirectUri = $request->getUri()->withQuery('');
+
         $query = $request->getQueryParams();
+
+        /*
+            Временный код, полученный после прохождения авторизации.
+         */
         $code = isset($query['code']) ? $query['code'] : '';
 
         /** @var SessionStorage $session */
@@ -63,16 +125,13 @@ class MainController extends Controller
                 'redirect_uri' => $redirectUri->__toString(),
                 'client_secret' => $clientSecret
             ]));
-        $result = file_get_contents($url->__toString());
+        $result = @file_get_contents($url->__toString());
         if(!$result){
             return $response->withHeader('Location', $sessionErrorRedirect->__toString());
         }
-        $resultArray = (array) json_decode($result);
-        $response->getBody()->write($result);
-        return $response->withHeader(
-            'Location',
-                $sessionRedirect->withQuery(http_build_query($resultArray))
-                    ->__toString()
-        );
+        $resultObj = json_decode($result);
+        $resultObj->redirect = $sessionRedirect->__toString();
+        $response->getBody()->write(json_encode($resultObj));
+        return $response;
     }
 }
